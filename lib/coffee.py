558979a298faf047b47cf7f8a9e1cfc6eb534e0a
Sheba <bathsheba.nyairo@student.moringaschool.com>
2026-03-29 00:01:02 +0300

    Initial commit

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+    # 1. Getter for size
+    @property
+    def size(self):
+        return self._size
+
+    # 2. Setter for size 
+    @size.setter
+    def size(self, value):
+        if value in ["Small", "Medium", "Large"]:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
